b9330592aa97839b178622f4f469bb8e97fa2795
nepton <nepton@qq.com>
2022-10-20 09:46:09 +0800

    nuget publish abstractions

---
+++ src/WeChatApi.Abstractions/nuget-publish.bat
@@ -0,0 +1,5 @@
+﻿echo Start publishing.............
+del bin\Release\*.nupkg
+del bin\Release\*.snupkg
+dotnet build -c Release
+dotnet nuget push bin\Release\*.nupkg --source https://api.nuget.org/v3/index.json 
